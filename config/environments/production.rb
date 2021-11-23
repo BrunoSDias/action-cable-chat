8411f7572122b640b853574f70efa23ec35b85c4
bruno <brunosdias@outlook.com>
2021-11-22 21:43:34 -0300

    websocket url && action cable allowed origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.web_socket_server_url = "wss://tsup-chat-app.herokuapp.com/"
+  config.action_cable.allowed_request_origins = ["https://tsup-chat-app.herokuapp.com/"]
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
